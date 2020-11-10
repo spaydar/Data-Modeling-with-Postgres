57d60873bd8a25990ced6266944ab1e07fd00651
Seth Paydar <sethpaydar@sbcglobal.net>
2020-11-10 06:22:45 +0000

    First attempt implementing SQL queries to drop, create, and insert into tables

--- sql_queries.py
+++ sql_queries.py
@@ -1,44 +1,54 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
-
-songplay_table_create = ("""
+# TODO - NOT SURE IF USER_ID SHOULD BE INT OR VARCHAR - IT IS PROVIDED AS A STRING IN THE JSON LOG BUT IS A NUMBER
+songplay_table_create = (""" 
+    CREATE TABLE IF NOT EXISTS songplays (songplay_id int PRIMARY KEY, start_time timestamp, user_id varchar, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (user_id varchar PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar);
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, title varchar, artist_id varchar, year int);
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude numeric, longitude numeric);
 """)
-
+# TODO - NOT SURE WHICH OF THE TIME UNITS SHOULD BE REPRESENTED AS INT, VARCHAR, OR TIMESTAMP 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (start_time timestamp, hour int, day int, week int, month int, year int, weekday boolean);
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
+    INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s);
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year) VALUES (%s, %s, %s, %s);
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s);
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
