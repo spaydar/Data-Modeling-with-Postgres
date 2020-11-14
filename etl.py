41526c1fc55cbda083a03ee38c4da4ed959d1c2f
Seth Paydar <sethpaydar@sbcglobal.net>
2020-11-14 06:55:51 +0000

    Added docstring comments to etl.py and removed inline comments.

--- etl.py
+++ etl.py
@@ -6,32 +6,44 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
-    # open song file
+    '''
+    Reads in a song-data file into a pandas dataframe, extracts required fields, and inserts data into the `songs` and `artists` tables in the database.
+    
+        Parameters:
+            cur: A cursor used to execute SQL commands over an open connection to a PostgreSQL database
+            filepath: The path of the song data file on the local filesystem
+    '''
+    
     df = pd.read_json(filepath, lines=True)
 
-    # insert song record
     song_data = list(df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0])
     cur.execute(song_table_insert, song_data)
     
-    # insert artist record
     artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0])
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
-    # open log file
+    '''
+    Reads in a user-activity log file into a pandas dataframe, filters for songplay records, converts the timestamp format from millisecond to datetime, 
+        then loads several datetime attributes into the `time` table in the database. 
+    Then reads in same log file to extract user-related fields to insert into the `users` table in the database.
+    Finally joins `songs` and `artists` tables on song title, artist name, and song duration to select song_id and artist_id for each row in the first
+        songplays dataframe to insert records into the `songplays` table in the database.
+        
+        Parameters:
+            cur: A cursor used to execute SQL commands over an open connection to a PostgreSQL database
+            filepath: The path of the user-activity data file on the local filesystem
+    '''
+    
     df = pd.read_json(filepath, lines=True)
 
-    # filter by NextSong action
     df = df[df['page']=="NextSong"]
 
-    # convert timestamp column to datetime
     df['ts'] = pd.to_datetime(df['ts'], unit='ms')
     
-    # extract the timestamp column
     t = pd.DataFrame({"start_time": df['ts']})
     
-    # insert time data records
     time_data = [[row[0], row.dt.hour[0], row.dt.day[0], row.dt.weekofyear[0], row.dt.month[0], row.dt.year[0], row.dt.weekday[0]] for i, row in t.iterrows()]
     column_labels = ['start_time','hour','day','week','month','year','weekday']
     time_df = pd.DataFrame(time_data, columns=column_labels)
@@ -39,17 +51,13 @@ def process_log_file(cur, filepath):
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
-    # load user table
     user_df = pd.read_json(filepath, lines=True)[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
-    # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
-    # insert songplay records
     for index, row in df.iterrows():
         
-        # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
         
@@ -58,24 +66,30 @@ def process_log_file(cur, filepath):
         else:
             songid, artistid = None, None
 
-        # insert songplay record
         songplay_data = [row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
-    # get all files matching extension from directory
+    '''
+    Retrieves all JSON files in the specified directory, prints the number of files found, then applies the provided `func` function to each file.
+    
+        Parameters:
+            cur: A cursor used to execute SQL commands over an open connection to a PostgreSQL database. Passed to `func` function
+            conn: An active connection to a PostgreSQL database used to commit changes made by `cur`
+            filepath: The directory to search for JSON files to apply `func` to
+            func: A function that accepts a cursor to a database connection and a file path
+    '''
+
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
         for f in files :
             all_files.append(os.path.abspath(f))
 
-    # get total number of files found
     num_files = len(all_files)
     print('{} files found in {}'.format(num_files, filepath))
 
-    # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
@@ -83,6 +97,9 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    '''
+    Opens a connection to the Sparkify database and gets a cursor to it, performs an ETL on the song and user-activity log files, then closes the connection.
+    '''
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
